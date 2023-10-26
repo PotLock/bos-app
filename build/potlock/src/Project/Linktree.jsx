95a0b8583b5e70132d807cd7e833a5c1d29b7af1
Lachlan Glen <54282009+lachlanglen@users.noreply.github.com>
2023-10-26 17:14:12 -0400

    fix linktree padding

--- build/potlock/src/Project/Linktree.jsx
+++ build/potlock/src/Project/Linktree.jsx
@@ -9,15 +9,17 @@ const LinktreeContainer = styled.div`
   flex-direction: column;
   align-items: flex-start;
   justify-content: flex-start;
+  background-color: pink;
   gap: 16px;
   width: 100%;
+  padding: 0px 14px;
 `;
 
 const LinktreeItemContainer = styled.div`
   padding: 8px 0px;
   display: flex;
   flex-direction: row;
-  // background-color: yellow;
+  background-color: yellow;
   align-items: center;
   justify-content: flex-start;
 `;
