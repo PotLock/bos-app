be3eaabe293f2ab44da5a654dd7f9939da102979
Lachlan Glen <54282009+lachlanglen@users.noreply.github.com>
2023-10-31 08:20:15 -0400

    fix category & deposit amount

--- apps/potlock/widget/Project/ListPage.jsx
+++ apps/potlock/widget/Project/ListPage.jsx
@@ -86,6 +86,17 @@ State.init({
   getRegisteredProjectsError: "",
 });
 
+const CATEGORY_MAPPINGS = {
+  "social-impact": "Social Impact",
+  "non-profit": "NonProfit",
+  climate: "Climate",
+  "public-good": "Public Good",
+  "de-sci": "DeSci",
+  "open-source": "Open Source",
+  community: "Community",
+  education: "Education",
+};
+
 if (!state.registeredProjects) {
   Near.asyncView(registryId, "get_projects", {})
     .then((projects) => {
@@ -118,7 +129,7 @@ if (!state.registeredProjects) {
             description: profileData.description ?? "",
             bannerImageUrl,
             profileImageUrl,
-            tags: [profileData.category.text ?? ""], // TODO: change this to get tags from horizon/social
+            tags: [CATEGORY_MAPPINGS[profileData.category] ?? ""], // TODO: get tags from horizon/social instead?
           };
           return formatted;
         });
