3769ee1bb7719a29a4f682a98118f1bf2dd4240f
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2023-12-29 12:40:00 -0600

    adjustments

--- apps/potlock/widget/Pots/Home.jsx
+++ apps/potlock/widget/Pots/Home.jsx
@@ -86,23 +86,23 @@ return (
     {state.pots && (
       <Widget
         src={`${ownerId}/widget/Components.ListSection`}
-        loading={
-          <div
-            style={{
-              width: "320px",
-              height: "200px",
-              borderRadius: "12px",
-              background: "white",
-              boxShadow: "0px -2px 0px #464646 inset",
-              border: "1px solid #292929",
-            }}
-          />
-        }
         props={{
           items: state.pots,
           renderItem: (pot) => (
             <Widget
               src={`${ownerId}/widget/Pots.Card`}
+              loading={
+                <div
+                  style={{
+                    width: "320px",
+                    height: "500px",
+                    borderRadius: "12px",
+                    background: "white",
+                    boxShadow: "0px -2px 0px #464646 inset",
+                    border: "1px solid #292929",
+                  }}
+                />
+              }
               props={{
                 ...props,
                 potId: pot.id,
