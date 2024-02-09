0b47433fc4e891e5eb53b6ab1597f52a1a32a1b3
Lachlan Glen <54282009+lachlanglen@users.noreply.github.com>
2024-02-09 16:24:14 -0500

    fix sponsors

--- apps/potlock/widget/Pots/Sponsors.jsx
+++ apps/potlock/widget/Pots/Sponsors.jsx
@@ -1,99 +1,170 @@
 // get donations
-const { ownerId, potId, potDetail } = props;
+const {
+  ownerId,
+  potId,
+  potDetail,
+  SUPPORTED_FTS: { NEAR },
+} = props;
 
-const TitleText = styled.div`
-  color: #3d3d3d;
-  font-size: 24px;
-  font-weight: 600;
-  line-height: 32px;
-  word-wrap: break-word;
+State.init({
+  donations: null,
+});
+
+if (!state.allDonations) {
+  Near.asyncView(potId, "get_matching_pool_donations", {}).then((donations) => {
+    // sort by size)
+    donations.sort(
+      (a, b) => NEAR.fromIndivisible(b.total_amount) - NEAR.fromIndivisible(a.total_amount)
+    );
+    // add % share of total to each donation
+    const total = NEAR.fromIndivisible(potDetail.matching_pool_balance);
+    donations = donations.map((donation) => {
+      return {
+        ...donation,
+        percentage_share: (NEAR.fromIndivisible(donation.net_amount) / total) * 100,
+      };
+    });
+    State.update({ donations });
+  });
+}
+
+if (!state.donations) return "Loading...";
+
+const columns = ["Rank", "Donor", "Amount", "Percentage Share"];
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: 24px;
+  width: 100%;
 `;
 
-const RowOuter = styled.div`
+const OuterTextContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+`;
+
+const TableContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 0.5px rgba(41, 41, 41, 0.5) solid;
+  box-shadow: 0px 4px 12px -4px rgba(82, 82, 82, 0.2);
+  border-radius: 2px;
+  width: 100%;
+`;
+
+const Header = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  padding: 12px 0px;
-  //   gap: 92px;
-  border-bottom: 1px #f0f0f0 solid;
+  background: #f6f5f3;
   width: 100%;
 `;
 
-const RowInner = styled.div`
+const HeaderItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: space-between;
+  justify-content: flex-start;
+  padding: 10px 20px;
+  width: ${95 / columns.length}%;
+`;
+
+const HeaderItemText = styled.div`
+  color: #292929;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 24px;
+  word-wrap: break-word;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const RowItem = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: flex-start;
-  gap: 10px;
+  gap: 20px;
+  padding: 20px;
+  width: ${95 / columns.length}%;
 `;
 
 const RowText = styled.div`
-  color: #525252;
-  font-size: 20px;
-  font-weight: 600;
-  line-height: 32px;
+  color: #292929;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 24px;
   word-wrap: break-word;
-  width: 100%;
 `;
-
-const matchingPoolDonations = Near.view(potId, "get_matching_pool_donations", {});
-
-if (!matchingPoolDonations) return "Loading...";
-
-const matchingPoolDonors = useMemo(() => {
-  const donors = {};
-  matchingPoolDonations.forEach((donation) => {
-    const { donorId } = donation;
-    donors[donorId] = true;
-  });
-  return Object.keys(donors);
-}, [matchingPoolDonations]);
 
 const { base_currency } = potDetail;
 
-const columns = ["Sponsor", "Amount", "Date"];
+const maxRowItemLength = 14;
 
 return (
-  <>
-    <props.ToDo>Add messages from donations?</props.ToDo>
-    <RowOuter>
-      {columns.map((column, index) => (
-        <TitleText key={index}>{column}</TitleText>
-      ))}
-    </RowOuter>
-    {matchingPoolDonations.map((donation, index) => {
-      const { message, donor_id, total_amount, donated_at } = donation;
-      const totalDonationAmount =
-        props.SUPPORTED_FTS[base_currency.toUpperCase()].fromIndivisible(total_amount);
+  <Container>
+    <TableContainer>
+      <Header>
+        {columns.map((column, index) => (
+          <HeaderItem style={index === 0 ? { width: "5%" } : {}}>
+            <HeaderItemText key={index}>{column}</HeaderItemText>
+          </HeaderItem>
+        ))}
+      </Header>
+      {state.donations.length === 0 ? (
+        <Row style={{ padding: "12px" }}>No donations to display</Row>
+      ) : (
+        state.donations.map((donation, index) => {
+          const { donor_id, total_amount, donated_at, percentage_share } = donation;
+          const totalDonationAmount =
+            props.SUPPORTED_FTS[base_currency.toUpperCase()].fromIndivisible(total_amount);
 
-      return (
-        <RowOuter key={index}>
-          <RowInner>
-            <Widget
-              src={`${ownerId}/widget/Project.ProfileImage`}
-              props={{
-                ...props,
-                accountId: donor_id,
-                imageWrapperStyle: {
-                  height: "32px",
-                  width: "32px",
-                },
-              }}
-            />
-            <RowText>{donor_id}</RowText>
-          </RowInner>
-          <RowInner>
-            <RowText>
-              {totalDonationAmount} {base_currency.toUpperCase()}
-            </RowText>
-          </RowInner>
-          <RowInner>
-            <RowText>{props.daysAgo(donated_at)}</RowText>
-          </RowInner>
-        </RowOuter>
-      );
-    })}
-    <props.ToDo>Add pagination</props.ToDo>
-  </>
+          return (
+            <Row key={index}>
+              <RowItem style={{ width: "5%" }}>
+                <RowText>#{index + 1}</RowText>
+              </RowItem>
+              <RowItem>
+                <Widget
+                  src={`${ownerId}/widget/Project.ProfileImage`}
+                  props={{
+                    ...props,
+                    accountId: donor_id,
+                    style: {
+                      height: "24px",
+                      width: "24px",
+                    },
+                  }}
+                />
+                <RowText>
+                  {donor_id.length > maxRowItemLength
+                    ? donor_id.slice(0, maxRowItemLength) + "..."
+                    : donor_id}
+                </RowText>
+              </RowItem>
+              <RowItem>
+                <RowText>
+                  {totalDonationAmount} {base_currency.toUpperCase()}
+                </RowText>
+              </RowItem>
+              <RowItem>
+                <RowText>{percentage_share.toFixed(0)}%</RowText>
+              </RowItem>
+            </Row>
+          );
+        })
+      )}
+    </TableContainer>
+  </Container>
 );
