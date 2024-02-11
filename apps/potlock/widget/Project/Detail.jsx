db438c2f19baef09363b50b7dd9d4dde4cb6eb73
Lachlan Glen <54282009+lachlanglen@users.noreply.github.com>
2024-02-11 11:11:56 -0500

    add project status banner

--- apps/potlock/widget/Project/Detail.jsx
+++ apps/potlock/widget/Project/Detail.jsx
@@ -4,11 +4,13 @@ const profile = props.profile ?? Social.getr(`${projectId}/profile`);
 
 const project = Near.view(REGISTRY_CONTRACT_ID, "get_project_by_id", { project_id: projectId });
 
-if (!profile || !project) {
+if (!profile || project == null) {
   return "Loading";
 }
 
-const projectIsViewable = project.status === "Approved" || userIsRegistryAdmin;
+if (project == undefined) {
+  return "Project not found";
+}
 
 const name = profile.name || "No-name profile";
 const image = profile.image;
@@ -51,6 +53,50 @@ const BodyContainer = styled.div`
   flex: 1;
 `;
 
+const Banner = styled.div`
+  width: 100%;
+  // max-height: 50px;
+  background: ${project.status === "Pending" ? "#E6B800" : "#dd3345"};
+  // background: rgb(6 10 15);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 8px 0;
+  margin-bottom: 8px;
+  border-radius: 4px;
+  // border-bottom: 2px rgb(96, 100, 102) solid;
+`;
+
+const BannerText = styled.div`
+  text-align: center;
+  color: white;
+  font-size: 16px;
+  font-weight: 600;
+  margin: 0 8px;
+  word-break: break-all;
+
+  @media screen and (max-width: 768px) {
+    font-size: 12px;
+    margin-left: 4px;
+  }
+`;
+
+const BannerAlertSvg = styled.svg`
+  width: 18px;
+
+  @media screen and (max-width: 768px) {
+    width: 14px;
+  }
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
 // these will be passed down to child components
 props.navOptions = [
   {
@@ -91,66 +137,91 @@ const profileImageTranslateYPx = 220;
 
 return (
   <Wrapper>
-    {!projectIsViewable ? (
-      <div style={{ textAlign: "center", paddingTop: "12px" }}>Project not found</div>
-    ) : (
-      <>
+    {project.status !== "Approved" && (
+      <Banner>
+        <Row>
+          <BannerAlertSvg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="white"
+            aria-hidden="true"
+            // width="18px"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+            ></path>
+          </BannerAlertSvg>
+          <BannerText>
+            This project status is {project.status} and has not been approved.
+          </BannerText>
+        </Row>
+        {project.review_notes && (
+          <BannerText style={{ fontStyle: "italic" }}>
+            Admin review notes: {project.review_notes}
+          </BannerText>
+        )}
+      </Banner>
+    )}
+    <Widget
+      src={`${ownerId}/widget/Project.BannerHeader`}
+      props={{
+        ...props,
+        profile,
+        // profileImageTranslateYPx,
+        // containerStyle: {
+        //   paddingLeft: "64px",
+        // },
+        backgroundStyle: {
+          objectFit: "cover",
+          left: 0,
+          top: 0,
+          height: "280px",
+        },
+        // TODO: image styles will vary based on viewport width, so should default to being set in the widget. can also be overriden using imageStyle prop if desired.
+        // imageStyle: {
+        //   width: `${imageHeightPx}px`,
+        //   height: `${imageHeightPx}px`,
+        // },
+      }}
+    />
+    <Container>
+      {/* <div class="row align-items-start"> */}
+      <SidebarContainer
+      // class="col-3"
+      >
         <Widget
-          src={`${ownerId}/widget/Project.BannerHeader`}
+          src={`${ownerId}/widget/Components.NavOptions`}
+          props={{
+            ...props,
+          }}
+        />
+        <Widget
+          src={`${ownerId}/widget/Project.Linktree`}
+          props={{
+            ...props,
+            linktree: profile.linktree,
+          }}
+        />
+      </SidebarContainer>
+      <BodyContainer
+      // class="col-9"
+      >
+        <Widget
+          src={`${ownerId}/widget/Project.Body`}
           props={{
             ...props,
             profile,
-            // profileImageTranslateYPx,
-            // containerStyle: {
-            //   paddingLeft: "64px",
-            // },
-            backgroundStyle: {
-              objectFit: "cover",
-              left: 0,
-              top: 0,
-              height: "280px",
-            },
-            // TODO: image styles will vary based on viewport width, so should default to being set in the widget. can also be overriden using imageStyle prop if desired.
-            // imageStyle: {
-            //   width: `${imageHeightPx}px`,
-            //   height: `${imageHeightPx}px`,
-            // },
+            project,
           }}
         />
-        <Container>
-          {/* <div class="row align-items-start"> */}
-          <SidebarContainer
-          // class="col-3"
-          >
-            <Widget
-              src={`${ownerId}/widget/Components.NavOptions`}
-              props={{
-                ...props,
-              }}
-            />
-            <Widget
-              src={`${ownerId}/widget/Project.Linktree`}
-              props={{
-                ...props,
-                linktree: profile.linktree,
-              }}
-            />
-          </SidebarContainer>
-          <BodyContainer
-          // class="col-9"
-          >
-            <Widget
-              src={`${ownerId}/widget/Project.Body`}
-              props={{
-                ...props,
-                profile,
-                project,
-              }}
-            />
-          </BodyContainer>
-          {/* </div> */}
-        </Container>
-        {/* <BodyContainerMobile>
+      </BodyContainer>
+      {/* </div> */}
+    </Container>
+    {/* <BodyContainerMobile>
           <div class="col-12">
             <Widget
               src={`${ownerId}/widget/Project.Body`}
@@ -161,7 +232,5 @@ return (
             />
           </div>
         </BodyContainerMobile> */}
-      </>
-    )}
   </Wrapper>
 );
