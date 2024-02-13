622a5eff8a158a2d05d24f0d0e3c637279a9693d
M-RB3 <rabea9220@gmail.com>
2024-02-13 16:19:15 +0400

    change Add to cart in proejct to donate

--- apps/potlock/widget/Project/Actions.jsx
+++ apps/potlock/widget/Project/Actions.jsx
@@ -1,3 +1,6 @@
+const [isModalDonationOpen, setIsModalDonationOpen] = useState(false);
+const [isModalDonationSucessOpen, setIsModalDonationSucessOpen] = useState(false);
+
 const { ownerId } = props;
 const Container = styled.div`
   display: flex;
@@ -43,6 +46,22 @@ const SubRow2 = styled.div`
   }
 `;
 
+const DonationButton = styled.button`
+  padding: 12px 16px;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 20px;
+  background: #fef6ee;
+  border-radius: 6px;
+  border: none;
+  box-shadow: 0px -2px 0px 0px #464646 inset, 0px 0px 0px 1px #464646;
+  &:hover {
+    background: #dd3345;
+    color: #fff;
+  }
+`;
+
 // const FollowingMobile = styled.div`
 //   display: none;
 
@@ -80,7 +99,27 @@ return (
       />
     </SubRow1>
     <SubRow2>
-      <Widget src={`${ownerId}/widget/Cart.AddToCart`} props={{ ...props, showModal: true }} />
+      <DonationButton
+        onClick={(e) => {
+          e.preventDefault();
+          setIsModalDonationOpen(true);
+        }}
+      >
+        Donate
+      </DonationButton>
+      <Widget
+        src={`${props.ownerId}/widget/Components.ModalDonation`}
+        props={{
+          ...props,
+          transactionHashes: props.transactionHashes,
+          ownerId: props.ownerId,
+          projectId: props.projectId,
+          isModalDonationOpen: isModalDonationOpen,
+          isModalDonationSucessOpen: isModalDonationSucessOpen,
+          onClose: () => setIsModalDonationOpen(false),
+        }}
+      />
+      {/* <Widget src={`${ownerId}/widget/Cart.AddToCart`} props={{ ...props, showModal: true }} /> */}
     </SubRow2>
   </Container>
 );
