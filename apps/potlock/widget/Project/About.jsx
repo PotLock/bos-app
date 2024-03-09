44edc3b5c7f650604a24ca7e96ef137e34cc83b7
M-RB3 <rabea9220@gmail.com>
2024-03-09 19:59:47 +0400

    update about section

--- apps/potlock/widget/Project/About.jsx
+++ apps/potlock/widget/Project/About.jsx
@@ -87,64 +87,65 @@ const SmartContractWrapper = styled.div`
     }
   }
 `;
-const smartContracts = [
-  ["0xa2dD817c2fDc3a2996f1A5174CF8f1AaED466E82", "Ethereum (ETH)"],
-  ["0xa2dD817c2fDc3a2996f1A5174CF8f1AaED466E82", "Ethereum (ETH)"],
-];
-// profile.plSmartContracts
-//   ? Object.entries(JSON.parse(profile.plSmartContracts)).reduce(
-//       (accumulator, [chain, contracts]) => {
-//         // Iterate over each contract address in the current chain
-//         const contractsForChain = Object.keys(contracts).map((contractAddress) => {
-//           return [chain, contractAddress]; // Create an array with the chain and contract address
-//         });
+const smartContracts = profile.plSmartContracts
+  ? Object.entries(JSON.parse(profile.plSmartContracts)).reduce(
+      (accumulator, [chain, contracts]) => {
+        // Iterate over each contract address in the current chain
+        const contractsForChain = Object.keys(contracts).map((contractAddress) => {
+          return [chain, contractAddress]; // Create an array with the chain and contract address
+        });
 
-//         return accumulator.concat(contractsForChain); // Add the arrays for this chain to the accumulator
-//       },
-//       []
-//     )
-//   : [];
+        return accumulator.concat(contractsForChain); // Add the arrays for this chain to the accumulator
+      },
+      []
+    )
+  : [];
 
-const githubRepos = ["github.com/CCCLebanon", "github.com/CCCLebanon", "github.com/CCCLebanon"];
-// profile.plGithubRepos ? JSON.parse(profile.plGithubRepos) : [];
+const githubRepos = profile.plGithubRepos ? JSON.parse(profile.plGithubRepos) : [];
 
-const Github = () => (
-  <GithubWrapper>
-    {githubRepos.map((url) => (
-      <a href={url} target="_blank">
-        <svg
-          width="14"
-          height="14"
-          viewBox="0 0 14 14"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M1.5 0.5V2.5H10.09L0.5 12.09L1.91 13.5L11.5 3.91V12.5H13.5V0.5H1.5Z"
-            fill="#7B7B7B"
-          />
-        </svg>
-        <div className="url">{url}</div>
-      </a>
-    ))}
-  </GithubWrapper>
-);
+const Github = () =>
+  githubRepos.length > 0 ? (
+    <GithubWrapper>
+      {githubRepos.map((url) => (
+        <a href={url} target="_blank">
+          <svg
+            width="14"
+            height="14"
+            viewBox="0 0 14 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1.5 0.5V2.5H10.09L0.5 12.09L1.91 13.5L11.5 3.91V12.5H13.5V0.5H1.5Z"
+              fill="#7B7B7B"
+            />
+          </svg>
+          <div className="url">{url}</div>
+        </a>
+      ))}
+    </GithubWrapper>
+  ) : (
+    "None provided"
+  );
 
-const SmartContracts = () => (
-  <SmartContractWrapper>
-    {smartContracts.map(([contract, chain]) => {
-      return (
-        <div className="contract">
-          <Widget src={`${ownerId}/widget/Project.CopyIcon`} props={{ textToCopy: contract }} />
-          <div className="text">
-            <div className="address">{contract}</div>
-            <div className="chain">{chain}</div>
+const SmartContracts = () =>
+  smartContracts.length > 0 ? (
+    <SmartContractWrapper>
+      {smartContracts.map(([chain, contract]) => {
+        return (
+          <div className="contract">
+            <Widget src={`${ownerId}/widget/Project.CopyIcon`} props={{ textToCopy: contract }} />
+            <div className="text">
+              <div className="address">{contract}</div>
+              <div className="chain">{chain}</div>
+            </div>
           </div>
-        </div>
-      );
-    })}
-  </SmartContractWrapper>
-);
+        );
+      })}
+    </SmartContractWrapper>
+  ) : (
+    "None provided"
+  );
 
 const AboutItem = ({ title, text }) => (
   <Widget
@@ -175,7 +176,7 @@ return (
     <AboutItem title="Overview" text={<Markdown text={description} />} />
     <AboutItem title="Why we are a public good" text={plPublicGoodReason || "None provided"} />
     <Team />
-    <AboutItem title="Smart contracts" text={<Github />} />
-    <AboutItem title="Github repo(s)" text={<SmartContracts />} />
+    <AboutItem title="Github repo(s)" text={<Github />} />
+    <AboutItem title="Smart contracts" text={<SmartContracts />} />
   </Container>
 );
