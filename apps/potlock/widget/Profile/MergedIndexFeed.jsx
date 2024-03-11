7497e2ac782a033e86b7a5b3687d21dffb235b9f
M-RB3 <rabea9220@gmail.com>
2024-03-11 17:11:05 +0400

    update feed empty state

--- apps/potlock/widget/Profile/MergedIndexFeed.jsx
+++ apps/potlock/widget/Profile/MergedIndexFeed.jsx
@@ -227,7 +227,48 @@ if (reverse) {
   items.reverse();
 }
 
-const renderedItems = items.length === 0 ? "No posts to display" : items.map(cachedRenderItem);
+const NoResults = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 68px 105px;
+  border-radius: 12px;
+  background: #f6f5f3;
+  .text {
+    font-family: "Lora";
+    max-width: 290px;
+    font-size: 22px;
+    font-style: italic;
+    font-weight: 500;
+    color: #292929;
+  }
+  img {
+    width: 60%;
+  }
+  @media screen and (max-width: 768px) {
+    flex-direction: column-reverse;
+    padding: 24px 16px;
+    .text {
+      font-size: 16px;
+    }
+    img {
+      width: 100%;
+    }
+  }
+`;
+
+const renderedItems =
+  items.length === 0 ? (
+    <NoResults>
+      <div className="text">The project has not posted yet.</div>
+      <img
+        src="https://ipfs.near.social/ipfs/bafkreicwz5cuku3kxxp3wzldslpfzmfqgukpm2wwy7t7zuevkdp4gbl2uq"
+        alt="pots"
+      />
+    </NoResults>
+  ) : (
+    items.map(cachedRenderItem)
+  );
 
 return props.manual ? (
   <>
