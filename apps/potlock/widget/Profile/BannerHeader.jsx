e6448aab2fa720a1e10d86b2b5b90245c0b11daa
M-RB3 <rabea9220@gmail.com>
2024-03-14 20:55:17 +0400

    add edit profile button

--- apps/potlock/widget/Profile/BannerHeader.jsx
+++ apps/potlock/widget/Profile/BannerHeader.jsx
@@ -222,6 +222,7 @@ const BackgroundImageContainer = styled.div`
   img {
     object-fit: cover;
     height: 100%;
+    width: 100%;
   }
   @media screen and (max-width: 768px) {
     height: 264px;
