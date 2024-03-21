700e4712d833f89aeed3d820d9ccdf07c981ee30
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-03-21 00:36:53 -0400

    remove flag

--- playwright.config.js
+++ playwright.config.js
@@ -100,7 +100,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev -p 8080",
+    command: "npm run dev",
     port: 8080,
     reuseExistingServer: !process.env.CI,
   },
