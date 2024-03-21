1b29b2a773d14e933c4a815dc63dab955a69def4
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-03-20 23:31:55 -0400

    working tests

--- playwright-tests/tests/bosloaderenvironment.spec.js
+++ playwright-tests/tests/bosloaderenvironment.spec.js
@@ -1,20 +1,16 @@
 import { test, expect } from "@playwright/test";
 
-test("should find bos loader configuration in localstorage", async ({
-  page,
-}) => {
+test("should find bos loader configuration in localstorage", async ({ page }) => {
   await page.goto("/");
 
-  const localStoreFlags = await page.evaluate(() =>
-    localStorage.getItem("flags")
-  );
+  const localStoreFlags = await page.evaluate(() => localStorage.getItem("flags"));
   expect(localStoreFlags).toEqual(
-    JSON.stringify({ bosLoaderUrl: "http://127.0.0.1:3030" })
+    JSON.stringify({ bosLoaderUrl: "http://127.0.0.1:8080/api/loader" })
   );
 });
 
 test("should not get bos loader fetch error", async ({ page }) => {
-  await page.goto("/devhub.near/widget/app?page=feed");
+  await page.goto("/potlock.near/widget/Index?tab=projects");
   const bodyText = await page.textContent("body");
-  expect(bodyText).not.toContain("BOS Loader fetch error");
+  expect(bodyText).not.toContain('Source code for "potlock.near/widget/Index" is not found');
 });
