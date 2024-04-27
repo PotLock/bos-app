6194c118c8930cf99e800a06278d7054715b7744
Saad Iqbal <saadiqbal.dev@outlook.com>
2024-04-27 17:38:20 +0500

    code cleanup

---
+++ apps/potlock/widget/Components/Icons.jsx
@@ -0,0 +1,6 @@
+const { Volunteer } = VM.require("potlock.near/widget/Components.Icons.Volunteer");
+const { Component } = VM.require("potlock.near/widget/Components.Icons.Component");
+return {
+  Volunteer,
+  Component,
+};
